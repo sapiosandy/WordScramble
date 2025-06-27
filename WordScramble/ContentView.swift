60e80906eafc014aa5befa1ed0ed31e79036b875
Sandy Gomez <sgomez1029@gmail.com>
2025-06-27 12:40:24 -0700

    Day 30 - Adding to a list of words

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,18 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    
     var body: some View {
         
-    }
-    
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            // we found the file in our bundle
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string
-                print(fileContents)
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
             }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
+    }
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+
+        // extra validation to come
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
     }
 }
 
